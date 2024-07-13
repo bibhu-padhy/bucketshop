99521fc7be839abadef1ab03df74a776d388cecb
Bibhu Padhy <bibhupadhy140@gmai.com>
2024-07-13 12:17:19 +0530

    typo fix

--- src/app/meteora/[address]/page.tsx
+++ src/app/meteora/[address]/page.tsx
@@ -5,5 +5,5 @@ export default async function DLMM({ params }: { params: { address: string } }) 
   
   const pool = await getPoolDetails(params.address)
   
-  return <div>DLLM detail : {pool.name} </div>;
+  return <div>DLMM detail : {pool.name} </div>;
 }
